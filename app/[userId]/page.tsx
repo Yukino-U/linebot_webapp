d860e929ebd28879f9d2bbe85d67385a25c8cc71
YukinoU <yukino.12346789@gmail.com>
2023-05-23 01:25:47 +0900

    検索機能追加

--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { GoogleSpreadsheetService } from "./spreadsheet";
 import { Dispatch, useEffect, useState } from "react";
 import { GiNotebook } from "react-icons/gi";
+import { BsSearchHeart, BsSearchHeartFill } from "react-icons/bs";
 
 const fetchData = async (
   url: string,
@@ -40,6 +41,7 @@ export default function Article({ params }: { params: { userId: string } }) {
   const [name, setName] = useState("");
   const [database, setDatabase] = useState([[]]);
   const [num, setNum] = useState(0);
+  const [inputText, setInputText] = useState("");
   const url =
     "https://sheets.googleapis.com/v4/spreadsheets/1A0Atc9_5FyXO775Jpvu-aXwC72RWL-zUdboEGLfkFx4/values/" +
     params.userId +
@@ -61,10 +63,19 @@ export default function Article({ params }: { params: { userId: string } }) {
           </div>
           <GiNotebook size={32}></GiNotebook>
         </div>
+        <div className="px-1 py-2 flex gap-2 items-center justify-center">
+          <BsSearchHeartFill size={24} color="#FEA1A1"></BsSearchHeartFill>
+          <input
+            className="p-1 bg-[#FEA1A1]/50 rounded text-black/60"
+            value={inputText}
+            placeholder="レシピ名検索"
+            onChange={(e) => setInputText(e.target.value)}
+          ></input>
+        </div>
 
-        <div className="bg-white/50 rounded-xl px-3 pb-3 pt-1 ">
+        <div className="bg-white/50 rounded-xl px-3 pb-3 pt-3 ">
           {database.map((item, key) => {
-            if (num > key) {
+            if (num > key && String(item[4]).includes(inputText)) {
               return (
                 <Link href={params.userId + "/" + key} key={key}>
                   <div className="border-b-2 border-[#FEA1A1] border-double text-lg py-2 break-words">
