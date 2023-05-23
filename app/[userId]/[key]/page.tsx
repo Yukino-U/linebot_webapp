3139112dbef1e036779455a1cde30aa99bfc52c9
YukinoU <yukino.12346789@gmail.com>
2023-05-23 12:27:06 +0900

    アイコン追加

--- app/[userId]/[key]/page.tsx
+++ app/[userId]/[key]/page.tsx
@@ -6,6 +6,8 @@ import { GoogleSpreadsheetService } from "../spreadsheet";
 import { Dispatch, useEffect, useState } from "react";
 import { GiCook } from "react-icons/gi";
 // }
+import HashLoader from "react-spinners/HashLoader";
+import Image from "next/image";
 
 async function hoge(params?: string) {
   //   const pink = "#FEA1A1";
@@ -14,7 +16,12 @@ async function hoge(params?: string) {
   //   console.log(spreadService.getTitle())
 }
 
-const fetchData = async (url: string, setName: any, setValues: any) => {
+const fetchData = async (
+  url: string,
+  setName: any,
+  setValues: any,
+  setLoading: any
+) => {
   try {
     const res = await fetch(url);
     if (!res.ok) {
@@ -28,10 +35,7 @@ const fetchData = async (url: string, setName: any, setValues: any) => {
     const disp_name = values[3][0];
     setName(disp_name);
     setValues(values);
-    // console.log(disp_name + "foo");
-    // console.log(values[0])
-    // return disp_name,values;
-    //   return values;
+    setLoading(false);
   } catch (err) {
     console.error(err);
   }
@@ -45,6 +49,7 @@ export default function Article({
   console.log("fuga");
   const [name, setName] = useState("");
   const [database, setDatabase] = useState([[]]);
+  const [loading, setLoading] = useState(true);
   const recipekey: number = params.key;
   const url =
     "https://sheets.googleapis.com/v4/spreadsheets/1A0Atc9_5FyXO775Jpvu-aXwC72RWL-zUdboEGLfkFx4/values/" +
@@ -52,7 +57,7 @@ export default function Article({
     "?key=AIzaSyCWex2ftB2_kLwKd54Ywh_fhkAwGDh-164";
   // console.log(recipekey)
   useEffect(() => {
-    fetchData(url, setName, setDatabase);
+    fetchData(url, setName, setDatabase, setLoading);
   }, [url]);
   //   console.log(database);
   //   const how = database[recipekey];
@@ -60,30 +65,34 @@ export default function Article({
 
   return (
     <div>
-      <Link href={params.userId}>
-        {" "}
-        <div className="bg-[#FEA1A1] h-20 text-white font-bold items-center flex text-2xl px-5">
-          Ai Recipe
-        </div>
-      </Link>
-
-      <div className="p-3">
-        {database.map((item, key) => {
-          if (key == recipekey) {
-            return (
-              <div key={key} className="bg-white/50 rounded-lg p-3">
-                <div className="flex text-lg font-bold text-black/80 items-end pt-1">
-                  <div className="border-b border-b-black/80 border-double px-1">
-                    {item[4]} の作り方
-                  </div>
-                  <GiCook size={24}></GiCook>
-                </div>
-                <div className="whitespace-pre-line py-2">{item[5]}</div>
-              </div>
-            );
-          }
-        })}
+      <div className="bg-[#FEA1A1] px-3 py-3">
+        <Link href={params.userId}>
+          <Image src="/airecipe_logo.png" alt="logo" height={40} width={200} />
+        </Link>
       </div>
+      {loading ? (
+        <div className="h-screen w-screen flex items-center justify-center">
+          <HashLoader color="#FEA1A1" className="" />
+        </div>
+      ) : (
+        <div className="p-3">
+          {database.map((item, key) => {
+            if (key == recipekey) {
+              return (
+                <div key={key} className="bg-white/50 rounded-lg p-3">
+                  <div className="flex text-lg font-bold text-black/80 items-end pt-1">
+                    <div className="border-b border-b-black/80 border-double px-1">
+                      {item[4]} の作り方
+                    </div>
+                    <GiCook size={24}></GiCook>
+                  </div>
+                  <div className="whitespace-pre-line py-2">{item[5]}</div>
+                </div>
+              );
+            }
+          })}
+        </div>
+      )}
     </div>
   );
 }
--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -6,12 +6,15 @@ import { GoogleSpreadsheetService } from "./spreadsheet";
 import { Dispatch, useEffect, useState } from "react";
 import { GiNotebook } from "react-icons/gi";
 import { BsSearchHeart, BsSearchHeartFill } from "react-icons/bs";
+import { HashLoader } from "react-spinners";
+import Image from "next/image";
 
 const fetchData = async (
   url: string,
   setName: any,
   setValues: any,
-  setNum: any
+  setNum: any,
+  setLoading: any
 ) => {
   try {
     const res = await fetch(url);
@@ -27,6 +30,7 @@ const fetchData = async (
     setName(disp_name);
     setValues(values);
     setNum(values[5][0]);
+    setLoading(false);
     // console.log(disp_name + "foo");
 
     // return disp_name,values;
@@ -42,51 +46,60 @@ export default function Article({ params }: { params: { userId: string } }) {
   const [database, setDatabase] = useState([[]]);
   const [num, setNum] = useState(0);
   const [inputText, setInputText] = useState("");
+  const [loading, setLoading] = useState(true);
   const url =
     "https://sheets.googleapis.com/v4/spreadsheets/1A0Atc9_5FyXO775Jpvu-aXwC72RWL-zUdboEGLfkFx4/values/" +
     params.userId +
     "?key=AIzaSyCWex2ftB2_kLwKd54Ywh_fhkAwGDh-164";
 
   useEffect(() => {
-    fetchData(url, setName, setDatabase, setNum);
+    fetchData(url, setName, setDatabase, setNum, setLoading);
   }, [url]);
 
   return (
     <div>
-      <div className="bg-[#FEA1A1] h-20 text-white font-bold items-center flex text-2xl px-5">
-        Ai Recipe
+      <div className="bg-[#FEA1A1] px-3 py-3">
+        <Link href={params.userId}>
+          <Image src="/airecipe_logo.png" alt="logo" height={40} width={200} />
+        </Link>
       </div>
-      <div className="p-5">
-        <div className="flex items-end gap-2 text-lg font-bold text-black/80 pt-1 pb-3 px-3">
-          <div className="border-b px-1 h-full border-black/80">
-            {name}さんの登録したレシピ
+      {loading ? (
+        <div className="h-screen w-screen flex items-center justify-center">
+          <HashLoader color="#FEA1A1" className="" />
+        </div>
+      ) : (
+        <div className="p-5">
+          <div className="flex items-end gap-2 text-lg font-bold text-black/80 pt-1 pb-3 px-3">
+            <div className="border-b px-1 h-full border-black/80">
+              {name}さんの登録したレシピ
+            </div>
+            <GiNotebook size={32}></GiNotebook>
           </div>
-          <GiNotebook size={32}></GiNotebook>
-        </div>
-        <div className="px-1 py-2 flex gap-2 items-center justify-center">
-          <BsSearchHeartFill size={24} color="#FEA1A1"></BsSearchHeartFill>
-          <input
-            className="p-1 bg-[#FEA1A1]/50 rounded text-black/60"
-            value={inputText}
-            placeholder="レシピ名検索"
-            onChange={(e) => setInputText(e.target.value)}
-          ></input>
-        </div>
+          <div className="px-1 py-2 flex gap-2 items-center justify-center">
+            <BsSearchHeartFill size={24} color="#FEA1A1"></BsSearchHeartFill>
+            <input
+              className="p-1 bg-[#FEA1A1]/50 rounded text-black/60"
+              value={inputText}
+              placeholder="レシピ名検索"
+              onChange={(e) => setInputText(e.target.value)}
+            ></input>
+          </div>
 
-        <div className="bg-white/50 rounded-xl px-3 pb-3 pt-3 ">
-          {database.map((item, key) => {
-            if (num > key && String(item[4]).includes(inputText)) {
-              return (
-                <Link href={params.userId + "/" + key} key={key}>
-                  <div className="border-b-2 border-[#FEA1A1] border-double text-lg py-2 break-words">
-                    ・{item[4]}
-                  </div>
-                </Link>
-              );
-            }
-          })}
+          <div className="bg-white/50 rounded-xl px-3 pb-3 pt-3 ">
+            {database.map((item, key) => {
+              if (num > key && String(item[4]).includes(inputText)) {
+                return (
+                  <Link href={params.userId + "/" + key} key={key}>
+                    <div className="border-b-2 border-[#FEA1A1] border-double text-lg py-2 break-words">
+                      ・{item[4]}
+                    </div>
+                  </Link>
+                );
+              }
+            })}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
